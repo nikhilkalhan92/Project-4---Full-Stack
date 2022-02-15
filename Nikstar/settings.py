8c079650bbc88727477eba3c4b9fa62593b8f530
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2022-02-15 19:36:45 +0000

    adjust settings.py heroku link

--- Nikstar/settings.py
+++ Nikstar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['project-4-full-stack.herokuapp.com', 'local host']
+ALLOWED_HOSTS = ['project-4-full-stack.herokuapp.com', 'localhost']
 
 
 # Application definition
