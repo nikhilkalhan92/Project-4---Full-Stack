3d373470bf2542f7eb7ed179ae7cf5ab43a87038
nikhilkalhan92 <84812103+nikhilkalhan92@users.noreply.github.com>
2022-04-15 19:34:35 +0500

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('delete_comment/<int:pk>/', views.delete, name='delete_comment'),
+    path('update_comment/<int:pk>/', views.update_comment, name='update_comment'),
     
+]    
   
     
     
-]
 
