8977a8616d9d0f04a08717600102325bfebe6d33
nikhilkalhan92 <84812103+nikhilkalhan92@users.noreply.github.com>
2023-01-12 03:53:46 +0500

    Update settings.py

--- Nikstar/settings.py
+++ Nikstar/settings.py
@@ -34,7 +34,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['project-4-full-stack.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['project-4-full-stack.herokuapp.com', 'localhost','*']
 
 
 # Application definition
